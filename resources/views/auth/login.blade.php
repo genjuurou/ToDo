09a023e7e71441027ef50c7449b666522be77f2c
Genjuurou <xkazeshinix94@gmail.com>
2023-01-18 18:34:28 +0100

    login, register fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,29 +1,27 @@
 <x-layout title="Login">
-    <div class="h-screen flex items-center justify-center bg-gray-50">
+    <div class="h-screen flex items-center justify-center">
         <div>
             <header class="mb-6">
                 <x-logo class="text-7xl text-center" />
             </header>
-            <main class="p-12 pt-6 bg-white rounded-lg shadow-xl">
-                <div class="text-center pb-2 mb-6 border-solid border-b border-gray-300">
-                    <h1 class="font-bold text-3xl">Login</h1>
-                </div>
-                
+            <main class="p-12 bg-white rounded-md">
                 @if ($errors->any())
-                    <x-alert color="danger">
-                        <p>Email / Password is incorrect</p>
-                    </x-alert>
+                    <div class="mb-4">
+                        <x-alert color="danger">
+                            <p>Email / Password is incorrect</p>
+                        </x-alert>
+                    </div>
                 @endif
 
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
 
                     <div class="flex flex-col gap-4">
-                        <x-input type="email" name="email" id="email" title="Email" autofocus required />
-                        <x-input type="password" name="password" id="password" title="Password" required />
+                        <x-input type="email" name="email" id="email" label="Email" autofocus required />
+                        <x-input type="password" name="password" id="password" label="Password" required />
 
                         <div class="pt-4">
-                            <x-button>Login</x-button>
+                            <x-button class="w-full bg-gray-200">Login</x-button>
                         </div>
                     </div>
                 </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,37 +1,35 @@
 <x-layout title="Register">
-    <div class="h-screen flex items-center justify-center bg-gray-50">
+    <div class="h-screen flex items-center justify-center">
         <div>
             <header class="mb-6">
                 <x-logo class="text-7xl text-center" />
             </header>
-            <main class="p-12 pt-6 bg-white rounded-lg shadow-xl">
-                <div class="text-center pb-2 mb-6 border-solid border-b border-gray-300">
-                    <h1 class="font-bold text-3xl">Register</h1>
-                </div>
-                
+            <main class="p-12 bg-white rounded-md">
                 @if ($errors->any())
-                    <x-alert color="danger">
-                        <h2 class="font-bold text-lg">Validation Failed:</h2>
+                    <div class="mb-4">
+                        <x-alert color="danger">
+                            <h2 class="font-bold text-lg">Validation Failed:</h2>
 
-                        <ul class="list-disc list-inside">
-                            @foreach($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </x-alert>
+                            <ul class="list-disc list-inside">
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </x-alert>
+                    </div>
                 @endif
 
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
 
                     <div class="flex flex-col gap-4">
-                        <x-input type="text" name="name" id="name" title="Name" autofocus required maxlength="255" :validation-failed="$errors->has('name')" />
-                        <x-input type="email" name="email" id="email" title="Email" required :validation-failed="$errors->has('email')" />
-                        <x-input type="password" name="password" id="password" title="Password" required :validation-failed="$errors->has('password')" />
-                        <x-input type="password" name="password_confirmation" id="password_confirmation" title="Password Confirmation" required :validation-failed="$errors->has('password')" />
+                        <x-input type="text" name="name" id="name" label="Name" autofocus required maxlength="255" />
+                        <x-input type="email" name="email" id="email" label="Email" required :validation-failed="$errors->has('email')" />
+                        <x-input type="password" name="password" id="password" label="Password" required />
+                        <x-input type="password" name="password_confirmation" id="password_confirmation" label="Password Confirmation" required />
 
                         <div class="pt-4">
-                            <x-button>Register</x-button>
+                            <x-button class="w-full bg-gray-200">Register</x-button>
                         </div>
                     </div>
                 </form>
